2cb87e7abb6a253680afaad417cffa78e67fe798
klikknpro <80091502+klikknpro@users.noreply.github.com>
2022-02-14 20:35:08 +0100

    init setup

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+const fs = require("fs");
+const cors = require("cors");
+const port = 3000;
+
+app.use(express.json());
+app.use(cors());
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
