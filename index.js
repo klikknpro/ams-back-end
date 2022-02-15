a2fe5c5f9629a9cb065d8d6230f606f3c6c019c1
bsamu <bsamuel0220@gmail.com>
2022-02-15 18:24:04 +0100

    registration

--- index.js
+++ index.js
@@ -1,13 +1,32 @@
-const express = require("express");
-const app = express();
-const fs = require("fs");
-const cors = require("cors");
-const port = 3000;
+const cors = require('cors')
+const express = require('express')
+const fs = require('fs')
+const app = express()
+const port = 4000
 
-app.use(express.json());
-app.use(cors());
+app.use(cors())
+app.use(express.json())
 
+const users = require('./users.json')
+
+app.post('/api/signup', (req, res) => {
+  if (!req.body.name || !req.body.password) {
+    return res.status(400).json('Missing credentials')
+  }
+  const userExists = users.some(user => user.name === req.body.name)
+  if (userExists) {
+    return res.sendStatus(409)
+  }
+  const newUser = {
+    name: req.body.name,
+    password: req.body.password
+  }
+  users.push(newUser)
+
+  fs.writeFileSync('users.json', JSON.stringify(users, null, 4))
+  res.sendStatus(200)
+})
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
+  console.log(`Example app listening on port ${port}`)
+})
